7e1de19ea3c30b0d748fa0e617603e51c2d4302a
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-09-02 14:17:16 +0600

    adding new

--- TestStudent.java
+++ TestStudent.java
@@ -4,5 +4,11 @@ public class TestStudent {
 	public static void main(String[] args) {
 		Student s1  = new Student("Dipraj",58);
 		s1.displayInfo();
+		
+		Student s2  = new Student("Kalyan",55);
+		s2.displayInfo();
+		
+		Student s3  = new Student("Rahul Ghose",43);
+		s3.displayInfo();
 	}
 }
