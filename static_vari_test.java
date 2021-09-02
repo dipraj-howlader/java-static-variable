f5ee9526102d1f62b2965ea8bab93c3c492875d9
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-09-02 14:28:12 +0600

    static vari 2'

---
+++ static_vari_test.java
@@ -0,0 +1,16 @@
+package StaticKeyword;
+
+public class static_vari_test {
+	public static void main(String[] args) {
+		
+//		static_variable2 ob = new static_variable2();
+//		System.out.println(ob.uniName);
+		
+		// To use Static varibale , dont need to declare object
+
+		System.out.println(static_variable2.uniName);
+		
+				
+		
+	}
+}
---
+++ static_variable2.java
@@ -0,0 +1,11 @@
+package StaticKeyword;
+
+public class static_variable2 {
+	//non-static variable
+	
+	
+//	String uniName = "PSSSTU";
+	
+	static String uniName = "PSSSTU";
+	
+}
